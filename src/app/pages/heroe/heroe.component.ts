f88f7ef675ad9436bc5b20e855d3f5cfc2a00ccf
elkingutierrex <elkin.gutierrex@gmail.com>
2019-08-22 07:46:35 -0500

    214 - Informacion del estado de las peticiones

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -3,6 +3,10 @@ import { HeroeModel } from 'src/app/models/heroe.model';
 import { NgForm } from '@angular/forms';
 import { HeroesService } from 'src/app/services/heroes.service';
 
+import Swal from 'sweetalert2';
+import { Observable } from 'rxjs';
+import { type } from 'os';
+
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
@@ -24,19 +28,32 @@ export class HeroeComponent implements OnInit {
       return;
     }
 
-    if( this.heroe.id ){
-      this.heroesService.actualizarHeroe( this.heroe )
-        .subscribe( resp => {
-          console.log(resp);
-        })
+    Swal.fire({
+      title: 'Espere',
+      text: 'Guardando Información',
+      type: 'info',
+      allowOutsideClick: false
+    });
+    Swal.showLoading();
 
+    let peticion: Observable<any>;
+
+
+    if( this.heroe.id ){
+      peticion = this.heroesService.actualizarHeroe( this.heroe );
     } else {
-      this.heroesService.crearHeroe( this.heroe )
-      .subscribe( resp => {
-        console.log(resp);
-      });
+      peticion = this.heroesService.crearHeroe( this.heroe );
     }
 
+    peticion.subscribe ( resp => {
+
+      Swal.fire({
+        title: this.heroe.nombre,
+        text:"Se actualizo correctamente",
+        type: "success"
+      })
+
+    })
 
 
 
